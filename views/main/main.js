0747f7a87fe3b517385edd440b3ba13b8de7743f
yuttpan <yuttpanin@gmail.com>
2016-12-17 21:23:36 +0700

    grid

--- views/main/main.js
+++ views/main/main.js
@@ -5,8 +5,40 @@ angular.module('myApp')
 
 
 
+ $http.get("http://ksnhealth.ddns.net/mra_api/select_discharge.php")
+   .then(
+       function(response){
+         console.log(response.data)
+$scope.myData = response.data ;
+       }, 
+       function(response){
+         console.log(response)
+       }
+    )
+
+ $scope.gridOptions = { data: 'myData',
+
+ columnDefs: [{
+        field: 'an',
+        displayName: 'AN',
+        cellClass: 'grid-align'
+     }, {
+        field: 'dchdate',
+        displayName: 'วันที่จำหน่าย'
+    },
+    {
+        field: 'ddate',
+        displayName: 'จำนวนวัน'
+    },{
+        field: 'name',
+        displayName: 'สิทธิ'
+    }
+    
+    
+    ]
 
 
+ };
 
 
 
